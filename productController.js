58f8e6ea30978398ade82880ce23e01bb85536c7
PriaGupta <162948238+PriaGupta@users.noreply.github.com>
2024-07-04 14:33:40 +0530

    changes done

---
+++ productController.js
@@ -0,0 +1,47 @@
+const Product = require('../models/productModel');
+
+const uploadProduct = async (req, res) => {
+
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        res.send({ message: "Uploaded successfully" });
+    } catch (err) {
+        console.error("Error uploading product:", err);
+        res.status(500).send({ message: "Server error" });
+    }
+};
+
+const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.send(products);
+    } catch (err) {
+        console.error("Error fetching products:", err);
+        res.status(500).send({ message: "Server error" });
+    }
+};
+
+
+const updateProduct = async (req, res) => {
+    try {
+      const { name, price,category, description, image } = req.body;
+      const product = await Product.findByIdAndUpdate(req.params.id, { name, price,category, description, image },
+         { new: true });
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+  // Delete product
+  const deleteProductById = async (req, res) => {
+    try {
+      await Product.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: 'Product deleted' });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+module.exports = { uploadProduct, getProducts ,updateProduct,deleteProductById};
